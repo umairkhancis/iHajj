bf7ada7fd5b7e8bf74447375475214d6acf3d62f
umair.khan <umair.khan>
2018-08-02 02:12:38 +0300

    added location service and main activity

---
+++ app/src/main/java/com/hajjhackathon/ihajj/application/iHajjApplication.java
@@ -0,0 +1,14 @@
+package com.hajjhackathon.ihajj.application;
+
+import android.app.Application;
+
+/**
+ * Created by umair.khan on 8/2/18.
+ */
+public class iHajjApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
